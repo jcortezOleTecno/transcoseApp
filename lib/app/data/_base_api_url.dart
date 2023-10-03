484df49079fbdd4bae5dcecaa8d95e85fe3e2972
Abdiel Reyes <abdielcastll@gmail.com>
2023-10-03 17:14:47 -0400

    Apuntando a grupovemare.com

--- lib/app/data/_base_api_url.dart
+++ lib/app/data/_base_api_url.dart
@@ -1,2 +1,3 @@
 // ignore: constant_identifier_names
-const BASE_API_URL = 'https://vemare.ole.agency';
+// const BASE_API_URL = 'https://vemare.ole.agency';
+const BASE_API_URL = 'https://grupovemare.com';
