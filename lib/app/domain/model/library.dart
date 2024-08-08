0acc9cf820d15ad1b4a5459c6c78434b1df2a7ab
jesusdja <jesus.cortez.1991@gmail.com>
2024-08-08 13:21:23 -0400

    videos enbebidos - libreria

--- lib/app/domain/model/library.dart
+++ lib/app/domain/model/library.dart
@@ -9,6 +9,7 @@ class Library {
     this.visits,
     this.image,
     this.imagePrivate,
+    this.videos = const [],
   });
 
   int id;
@@ -20,6 +21,7 @@ class Library {
   int? visits;
   String? image;
   String? imagePrivate;
+  List<LibraryVideos> videos;
 
   factory Library.fromJson(dynamic json) => Library(
         id: json["id"] as int,
@@ -31,6 +33,8 @@ class Library {
         visits: json["visits"] as int?,
         image: json["image"] as String?,
         imagePrivate: json["image_private"] as String?,
+        videos: json['videos'] == null ? [] :
+        (json['videos'] as List).map((e) => LibraryVideos.fromJson(e)).toList(),
       );
 
   Map<String, dynamic> toJson() => {
@@ -42,5 +46,37 @@ class Library {
         "tags": tags,
         "visits": visits,
         "image_private": imagePrivate,
+        "videos": videos,
       };
+}
+
+class LibraryVideos {
+  final int? id;
+  final String? link;
+  final String? name;
+  final String? description;
+
+  LibraryVideos({
+    this.id,
+    this.link,
+    this.name,
+    this.description,
+  });
+  factory LibraryVideos.fromJson(dynamic map) {
+    return LibraryVideos(
+      id: map['id'] as int?,
+      link: map['link'] as String?,
+      name: map['name'] as String?,
+      description: map['description'] as String?,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'id': id,
+      'link': link,
+      'name': name,
+      'description': description,
+    };
+  }
 }
