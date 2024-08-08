537944dfc34151fa28c2a6155c3363f5b4e42643
jesusdja <jesus.cortez.1991@gmail.com>
2024-08-08 11:19:24 -0400

    cabeceras formaciones

--- lib/app/view/my_services/formations/formations/bloc/formations_cubit.dart
+++ lib/app/view/my_services/formations/formations/bloc/formations_cubit.dart
@@ -1,21 +1,29 @@
 import 'package:bloc/bloc.dart';
 import 'package:vemare/app/data/formations_repository.dart';
+import 'package:vemare/app/data/header_repository.dart';
 import 'package:vemare/app/domain/model/formation.dart';
+import 'package:vemare/app/domain/model/header.dart';
 import 'package:vemare/app/view/my_services/formations/formations/bloc/formations_state.dart';
 
 class FormationsCubit extends Cubit<FormationsState> {
   FormationsCubit(
-    this._formationsRepository,
-  ) : super(const FormationsState()) {
+      this._formationsRepository,
+      this._headerRepository,
+      ) : super(const FormationsState()) {
     fetchData();
   }
 
   final FormationsRepository _formationsRepository;
+  final HeaderRepository _headerRepository;
 
   Future<void> fetchData() async {
     emit(state.copyWith(loading: true));
-    List<TrainigGroup> formations =
-        await _formationsRepository.getTrainingGroup();
-    emit(state.copyWith(formations: formations, loading: false));
+    List<TrainigGroup> formations = await _formationsRepository.getTrainingGroup();
+    List<Header> headers = await _headerRepository.getHeaders();
+    emit(state.copyWith(
+      formations: formations,
+      loading: false,
+      headers: headers,
+    ));
   }
 }
--- lib/app/view/my_services/formations/formations/bloc/formations_state.dart
+++ lib/app/view/my_services/formations/formations/bloc/formations_state.dart
@@ -1,5 +1,6 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:vemare/app/domain/model/formation.dart';
+import 'package:vemare/app/domain/model/header.dart';
 import 'package:vemare/app/domain/value_object/status.dart';
 
 part 'formations_state.freezed.dart';
@@ -9,6 +10,7 @@ class FormationsState with _$FormationsState {
   const factory FormationsState({
     @Default(false) bool loading,
     @Default(FormStatus.editing) FormStatus status,
+    @Default(<Header>[]) List<Header> headers,
     @Default(<TrainigGroup>[]) List<TrainigGroup> formations,
   }) = _FormationsState;
   const FormationsState._();
