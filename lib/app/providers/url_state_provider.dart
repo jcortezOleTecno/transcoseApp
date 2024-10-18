aedf39aedae42708dfc8e1b45b1424b9401f4203
jesusdja <jesus.cortez.1991@gmail.com>
2024-10-18 10:32:19 -0400

    error al subirla a playstore por la version de compilacion de android

--- lib/app/providers/url_state_provider.dart
+++ lib/app/providers/url_state_provider.dart
@@ -1,9 +1,8 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
-// import 'package:flutter_multi_formatter/flutter_multi_formatter.dart';
-// import 'package:vemare/app/data/url_dynamic_repository.dart';
-// import 'package:vemare/config/service_locator.dart';
+import 'package:vemare/app/data/url_dynamic_repository.dart';
+import 'package:vemare/config/service_locator.dart';
 
 enum AppStatus {
   urlDynamic,app
@@ -16,13 +15,16 @@ class UrlDynamicProvider extends ChangeNotifier {
   Future<String> getUrl() async{
     String url = '';
     try{
-      // String totem = await getIt.get<UrlDynamicRepository>().getTotem() ?? '';
-      // log('totem: $totem');
-      // url = await getIt.get<UrlDynamicRepository>().getUrl(totem: totem) ?? '';
-      // if(url[url.length - 1] == '/' || url[url.length - 1] == '/'){
-      //   url = url.substring(0,(url.length - 1));
-      // }
+      String totem = await getIt.get<UrlDynamicRepository>().getTotem() ?? '';
+      log('totem: $totem');
+      url = await getIt.get<UrlDynamicRepository>().getUrl(totem: totem) ?? '';
+      if(url[url.length - 1] == '/' || url[url.length - 1] == '/'){
+        url = url.substring(0,(url.length - 1));
+      }
     }catch(_){}
+    if(url.isEmpty){
+      url = 'https://desarrollo.transcose.com';
+    }
     //url = 'https://transcose.oletecnologia.com';
     url = 'https://desarrollo.transcose.com';
     //url = 'https://www.transcose.com';
